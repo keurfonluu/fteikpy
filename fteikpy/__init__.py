ffb908c40e5418ecabd9b8ab2585ec01e2ac8a0f
Keurfon Luu <keurfonluu@outlook.com>
2020-11-01 23:04:46 -0800

    add shortcut to numba.set_num_threads

--- fteikpy/__init__.py
+++ fteikpy/__init__.py
@@ -1,8 +1,10 @@
+from ._helpers import set_num_threads
 from ._solver import EikonalSolver2D, EikonalSolver3D
 from .__about__ import __version__
 
 __all__ = [
     "EikonalSolver2D",
     "EikonalSolver3D",
+    "set_num_threads",
     "__version__",
 ]
---
+++ fteikpy/_helpers.py
@@ -0,0 +1,5 @@
+import numba
+
+
+def set_num_threads(n):
+    numba.set_num_threads(n)
