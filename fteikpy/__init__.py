638f8a52ea55877b80dc9092335171ee99b8d98a
Keurfon Luu <keurfonluu@outlook.com>
2020-11-10 23:42:35 -0800

    make grid classes public

--- fteikpy/__init__.py
+++ fteikpy/__init__.py
@@ -1,3 +1,4 @@
+from ._grid import Grid2D, Grid3D, TraveltimeGrid2D, TraveltimeGrid3D
 from ._helpers import set_num_threads
 from ._solver import EikonalSolver2D, EikonalSolver3D
 from .__about__ import __version__
@@ -5,6 +6,10 @@ from .__about__ import __version__
 __all__ = [
     "EikonalSolver2D",
     "EikonalSolver3D",
+    "Grid2D",
+    "Grid3D",
+    "TraveltimeGrid2D",
+    "TraveltimeGrid3D",
     "set_num_threads",
     "__version__",
 ]
